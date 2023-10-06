97cf7677ba09bc88dd73a2c25f8abd947e229fa4
MainaZaquir <mzaquir58@gmail.com>
2023-10-06 11:07:51 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+
+  if (distance <= 400) {
+    return 'This one is on me!';
+  }
+  else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500) {
+    return '+No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Example usage
+console.log(scuberGreetingForFeet("300")); // Output: 'This one is on me!'
+console.log(scuberGreetingForFeet("2300")); // Output: 'I will gladly take yor thirty bucks.'
+console.log(scuberGreetingForFeet("2800")); // Output: 'No can do' 
+
+  
+
+  function ternaryCheckCity(city) {
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Example usage:
+console.log(ternaryCheckCity("NYC")); // Output: 'Ok, sounds good.'
+console.log(ternaryCheckCity("LA")); // Output: 'No go.'
+
+
+  function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+
+    case "not as generous":
+      return "Thank you.";
+
+    default:
+      return "Bye.";
+  }
 }
+
+// Example usage:
+console.log(switchOnCharmFromTip("generous")); // Output: 'Thank you so much.'
+console.log(switchOnCharmFromTip("not as generous")); // Output: 'Thank you.'
+console.log(switchOnCharmFromTip("average")); // Output: 'Bye.'}
